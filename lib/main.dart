2c4e82831f30c70bdaa20bacbf5502d0d0ec4c4d
rinonkia <git@gatapon.net>
2020-07-23 20:06:25 +0900

    メニューらしいメニューが出来た

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,49 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const data = [Text("item0"), Text("item1"), Text("item3"), Text("item4")];
-    print(context);
     return MaterialApp(
       home: Scaffold(
-        body: ListView(children: data),
+        appBar: AppBar(
+          title: Text('ListView'),
+        ),
+        body: ListView(
+          children: [
+            _menuItem("メニュー1", Icon(Icons.settings)),
+            _menuItem("メニュー2", Icon(Icons.map)),
+            _menuItem("メニュー3", Icon(Icons.room)),
+            _menuItem("メニュー4", Icon(Icons.local_shipping)),
+            _menuItem("メニュー5", Icon(Icons.airplanemode_active)),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _menuItem(String title, Icon icon) {
+    return GestureDetector(
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        decoration: new BoxDecoration(
+          border: new Border(
+            bottom: BorderSide(width: 1.0, color: Colors.grey),
+          ),
+        ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: icon,
+            ),
+            Text(
+              title,
+              style: TextStyle(color: Colors.black, fontSize: 18.0),
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        print("onTap called.");
+      },
     );
   }
 }
