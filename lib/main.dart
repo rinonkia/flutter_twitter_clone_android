985d89988e0e921035996f3f2d808204ec4c3c9d
rinonkia <git@gatapon.net>
2020-07-24 16:46:25 +0900

    learn Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:developer' as developer;
 
 void main() {
   runApp(MyApp());
@@ -8,41 +7,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var list = ["メッセージ", "メッセージ", "メッセージ", "メッセージ", "メッセージ"];
     return MaterialApp(
+      title: 'Flutter Material Design',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('ListView'),
+        body: Center(
+          child: Container(
+            color: Colors.blue,
+            width: 300.0,
+            height: 300.0,
+            child: Text('word'),
+            alignment: Alignment.center,
+            transform: Matrix4.rotationZ(0.1),
+          ),
         ),
-        body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            print(index);
-            if (index >= list.length) {
-              list.addAll(["メッセージ", "メッセージ", "メッセージ", "メッセージ"]);
-            }
-            return _messageItem(list[index]);
-          },
-        ),
-      ),
-    );
-  }
-
-  Widget _messageItem(String title) {
-    return Container(
-      decoration: new BoxDecoration(
-        border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey)),
-      ),
-      child: ListTile(
-        title: Text(
-          title,
-          style: TextStyle(color: Colors.black, fontSize: 18.0),
-        ),
-        onTap: () {
-          print("onTap called.");
-        },
-        onLongPress: () {
-          print("onLongTap called.");
-        },
       ),
     );
   }
