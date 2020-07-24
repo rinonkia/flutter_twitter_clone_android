370849d922b4e0a07f527488523d7a6df7971407
rinonkia <git@gatapon.net>
2020-07-24 16:27:20 +0900

    change: エンドレススクロール

--- lib/main.dart
+++ lib/main.dart
@@ -8,31 +8,31 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var list = ["メッセージ", "メッセージ", "メッセージ", "メッセージ", "メッセージ"];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('ListView'),
         ),
-        body: ListView(
-          children: [
-            _menuItem("メニュー1", Icon(Icons.settings)),
-            _menuItem("メニュー2", Icon(Icons.map)),
-            _menuItem("メニュー3", Icon(Icons.room)),
-            _menuItem("メニュー4", Icon(Icons.local_shipping)),
-            _menuItem("メニュー5", Icon(Icons.airplanemode_active)),
-          ],
+        body: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            print(index);
+            if (index >= list.length) {
+              list.addAll(["メッセージ", "メッセージ", "メッセージ", "メッセージ"]);
+            }
+            return _messageItem(list[index]);
+          },
         ),
       ),
     );
   }
 
-  Widget _menuItem(String title, Icon icon) {
+  Widget _messageItem(String title) {
     return Container(
       decoration: new BoxDecoration(
         border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey)),
       ),
       child: ListTile(
-        leading: icon,
         title: Text(
           title,
           style: TextStyle(color: Colors.black, fontSize: 18.0),
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
           print("onTap called.");
         },
         onLongPress: () {
-          print("onLongPress called.");
+          print("onLongTap called.");
         },
       ),
     );
