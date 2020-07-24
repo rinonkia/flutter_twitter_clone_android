dc08024dab19ecd72d65a6aff98a4deb05f8c91a
rinonkia <git@gatapon.net>
2020-07-24 23:29:55 +0900

    add: レイアウトの色の情報をまとめたcolors.dartを追加した

---
+++ lib/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const twitterColor = const Color.fromRGBO(29, 161, 242, 1);
+const darkColor = const Color.fromRGBO(21, 32, 43, 1);
+const textColor = Colors.white;
