735906d605a2b1d066819884fe8c5a8d7641e1ee
rinonkia <git@gatapon.net>
2020-07-26 16:58:17 +0900

    githubを見ながらUserNameとメッセージを表示させた

--- lib/main.dart
+++ lib/main.dart
@@ -13,40 +13,59 @@ class MyApp extends StatelessWidget {
       title: 'Column & Row',
       home: Scaffold(
         appBar: AppBar(
-            title: Text('Welcome Twitter'),
-            backgroundColor: darkColor,
-            elevation: 1),
+          title: Text('Welcome Twitter'),
+          backgroundColor: darkColor,
+          elevation: 1,
+        ),
         body: ListView.builder(
           itemBuilder: (BuildContext context, int index) {
             if (index >= list.length) {
-              list.addAll(
-                  ["message", "message", "message", "message", "message"]);
+              list.addAll(["message", "message", "message", "message"]);
             }
-            return _messageItem(list[index]);
+            return _userProfilePhoto(list[index]);
           },
         ),
       ),
     );
   }
 
-  Widget _messageItem(String title) {
+  Widget _userProfilePhoto(title) {
     return Container(
-      decoration: new BoxDecoration(
-        border: new Border(bottom: BorderSide(width: 1.0, color: Colors.white)),
-      ),
-      child: ListTile(
-        leading: Image.asset('images/default.jpg'),
-        title: Text(
-          title,
-          style: TextStyle(color: Colors.black, fontSize: 18.0),
-        ),
-        contentPadding: EdgeInsets.all(10.0),
-        onTap: () {
-          print('onTap called.');
-        },
-        onLongPress: () {
-          print('onLongPress called.');
-        },
+      color: darkColor,
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage("images/default.jpg"),
+              ),
+              SizedBox(width: 15.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'UserName',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
