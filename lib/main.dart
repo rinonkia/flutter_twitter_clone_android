1432771420b0ad3c41e5f6c779b08b840c346efd
rinonkia <git@gatapon.net>
2020-07-27 23:54:44 +0900

    change: アイコンのサイズを拡大

--- lib/main.dart
+++ lib/main.dart
@@ -64,6 +64,7 @@ class MyApp extends StatelessWidget {
         bottomNavigationBar: BottomNavigationBar(
           unselectedItemColor: Colors.grey,
           backgroundColor: darkColor,
+          iconSize: 30.0,
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
