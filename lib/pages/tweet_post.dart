583baa833c979dcad9cd4f3ef28093b2daf0cbaf
rinonkia <git@gatapon.net>
2020-08-01 17:05:42 +0900

    change: tweet_post.dartのAppBarの見た目を作成

--- lib/pages/tweet_post.dart
+++ lib/pages/tweet_post.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
-// import 'package:document/colors.dart';
+import 'package:document/colors.dart';
 
 class TweetPost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Navigator'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            GestureDetector(
+              onTap: () => print("下書き tapped!"),
+              child: Text(
+                "下書き",
+                style: TextStyle(
+                  color: twitterColor,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(width: 18.0),
+            RaisedButton(
+              child: Text(
+                "ツイートする",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              color: twitterColor,
+              shape: StadiumBorder(),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        backgroundColor: darkColor,
       ),
       body: Container(
         padding: EdgeInsets.all(32.0),
